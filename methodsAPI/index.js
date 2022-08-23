c09dc086723612f22fa563a582ca6bfadd1866b3
italo1033 <italogammy1033@gmail.com>
2022-08-22 21:45:39 -0300

    criando metodo get e post

--- methodsAPI/index.js
+++ methodsAPI/index.js
@@ -1,3 +1,23 @@
 const express=require('express');
 
 const app= express();
+app.use(express.json())
+//as funções routes é justamente para onde os dados serão exibidos, assim eles vai receber o que foi colocado na url e dá um retorno
+app.route("/").get((req, res) => {
+    res.send("olá mundo")
+})
+
+
+app.route('/testePost').post((req,res) => {
+    body= req.body
+    res.send(body)
+})
+
+
+// listem é responsavel por ouvir o que você fez e manda para a porta que você designou.
+app.listen('3000')
+
+
+
+
+console.log("verde")
